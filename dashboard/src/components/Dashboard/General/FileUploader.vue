c755cd617953573ad8ff1b8fa8aea762a95a1aa9
programadorgrupomc <programador@grupomcperu.com>
2023-06-28 19:03:38 -0500

    Update FileUploader.vue

--- dashboard/src/components/Dashboard/General/FileUploader.vue
+++ dashboard/src/components/Dashboard/General/FileUploader.vue
@@ -1,28 +1,25 @@
 <template>
-	<cropper
-		:src="img"
-		@change="change"
-        
-	/>
+    <cropper :src="img" @change="change" />
 </template>
 
 <script>
+
 import { Cropper } from 'vue-advanced-cropper';
 import 'vue-advanced-cropper/dist/style.css';
 
 export default {
-	components: {
-		Cropper,
-	},
-	data() {
-		return {
-			img: 'https://images.pexels.com/photos/4323307/pexels-photo-4323307.jpeg',
-		};
-	},
-	methods: {
-		change({ coordinates, canvas }) {
-			console.log(coordinates, canvas);
-		},
-	},
+    components: {
+        Cropper,
+    },
+    data() {
+        return {
+            img: 'https://images.pexels.com/photos/4323307/pexels-photo-4323307.jpeg',
+        };
+    },
+    methods: {
+        change({ coordinates, canvas }) {
+            console.log(coordinates, canvas);
+        },
+    },
 };
 </script>
